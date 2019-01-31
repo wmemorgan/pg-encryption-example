4fc1e68d4a8c6476826451cf9a3a4801adf3ed40
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-01 00:04:57 +0800

    Add node modules that are dependent on swagger

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import jsonBody from 'koa-json-body'
 import { postgresMiddleware, postgres } from './postgres'
 import { schema, insert, retrieve, retrieveAll, update } from './models'
 
+import swaggerUi from 'swagger-ui-koa'
+import * as swagger from 'swagger2'
+
 // const Koa = require('koa')
 // const Router = require('koa-router')
 // const jsonBody = require('koa-json-body')
@@ -15,6 +18,14 @@ const app = new Koa()
   .use(postgresMiddleware(schema))
 const port = 9001 
 const router = new Router() 
+
+const spec = swagger.loadDocumentSync('./swagger.yaml')
+if (!swagger.validateDocument(spec)) {
+  throw Error(`Invalid Swagger File`)
+}
+
+app.use('/', swaggerUi.serve)
+app.get('/', swaggerUi.setup(spec))
 
 router 
   .post('/cards', async ctx => {
@@ -38,9 +49,16 @@ router
     await update(postgres(ctx), data.name, ctx.params.id)
     ctx.status = 204
   })
+  .get('/swagger.json', ctx => {
+    ctx.body = spec
+  })
 
   app.use(router.routes())
   app.use(router.allowedMethods())
+
+  app.use(postgresMiddleware())
+  app.use('/v1', router)
+
   app.listen(port, () => {
     console.log(`Server started at ${port}`)
   })
